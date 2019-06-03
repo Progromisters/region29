419c3885b78347bed51eb282c1856107e07fb548
LaDIp <laa2014@gmail.com>
2019-06-03 23:15:57 +0300

    Added slider

--- static/assets/js/common.js
+++ static/assets/js/common.js
@@ -1,1 +1,9 @@
-//fdfdfdsd
+$('.slider').slick({
+  infinite: true,
+  speed: 500,
+  autoplay: true,
+  autoplaySpeed: 5000,
+  //адаптивная высота    
+  prevArrow: '<button class="slick-arrow__slick-prev"><i class="fas fa-chevron-left"></i></button>',
+  nextArrow: '<button class="slick-arrow__slick-next"><i class="fas fa-chevron-right"></i></button>',
+});
