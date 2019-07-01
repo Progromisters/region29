d4c2a0c2c741c5a0dcd9195d1b02a8c0e15294e1
Altair <eclipsealtair@gmail.com>
2019-07-01 20:06:13 +0300

    change text price.html

--- main/urls.py
+++ main/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
     path('feedback', views.feedback, name='feedback'),
     path('цены', views.price, name='price'),
     path('отзывы', views.reviews, name='reviews'),
-    path('контакты', views.contacts, name='contacts'),
-    path('об-автошколе', views.about, name='about')
+    path('об-автошколе', views.about, name='about'),
+    path('контакты', views.contacts, name='contacts')
 ]
