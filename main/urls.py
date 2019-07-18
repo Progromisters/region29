e511d580e1e06001ff610e88caa66e2fc261a1a6
Altair <eclipsealtair@gmail.com>
2019-07-18 21:51:26 +0300

    fix manifest

--- main/urls.py
+++ main/urls.py
@@ -19,5 +19,6 @@ urlpatterns = [
     path('контакты', views.contacts, name='contacts'),
     path('robots.txt', lambda r: HttpResponse("User-agent: *\nDisallow: /admin", content_type="text/plain")),
     path('sitemap.xml', sitemap, {'sitemaps': sitemaps}, name='django.contrib.sitemaps.views.sitemap'),
-    path('manifest.json', lambda r: HttpResponse('{"name": "Автошкола Регион29","short_name": "Регион29","start_url": "/","display": "standalone"}', content_type="application/json"))
+    path('manifest.json', lambda r: HttpResponse('{"name": "Автошкола Регион29","short_name": "Регион29","start_url": "/",'
+                                                 '"display": "standalone","theme_color": "#000", "background_color": "#fff"}', content_type="application/json"))
 ]
