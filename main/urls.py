fe2423d3253accf964681ef9deccce72757d4497
Altair <eclipsealtair@gmail.com>
2019-07-31 18:54:24 +0300

    change robots

--- main/urls.py
+++ main/urls.py
@@ -17,7 +17,7 @@ urlpatterns = [
     path('отзывы', views.reviews, name='reviews'),
     path('об-автошколе', views.about, name='about'),
     path('контакты', views.contacts, name='contacts'),
-    path('robots.txt', lambda r: HttpResponse("User-agent: *\nDisallow: /admin", content_type="text/plain")),
+    path('robots.txt', lambda r: HttpResponse("User-agent: *\nDisallow: /admin\nSitemap: http://водитель29.рф/sitemap.xml", content_type="text/plain")),
     path('sitemap.xml', sitemap, {'sitemaps': sitemaps}, name='django.contrib.sitemaps.views.sitemap'),
     path('manifest.json', lambda r: HttpResponse('{"name": "Автошкола Регион29","short_name": "Регион29","start_url": "/",'
                                                  '"display": "standalone","theme_color": "#000", "background_color": "#fff"}', content_type="application/json"))
